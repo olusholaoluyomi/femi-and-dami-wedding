618807529530906f234867d9bc312f64b9e44ce1
Oluyomi, Olushola Michael <olusholaoluyomi@gmail.com>
2025-08-30 19:21:01 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ToastProvider } from './context/ToastContext'; // Add this import
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ToastProvider> {/* Wrap App with ToastProvider */}
+      <App />
+    </ToastProvider>
   </StrictMode>
 );
