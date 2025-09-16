073307d9c3a0c3b4ed5bf55a2906493f0a036f7c
Oluyomi, Olushola Michael <olusholaoluyomi@gmail.com>
2025-09-16 18:13:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 this makes assets load correctly on Vercel
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
